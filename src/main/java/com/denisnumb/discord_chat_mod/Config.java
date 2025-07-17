fe37f6ff75d96934e333b3087fcb883ae222ff21
Tyrthurey <tyrthurey@gmail.com>
2025-07-17 19:00:21 +0300

    - Added new configuration options for achievement message style and description inclusion in Config.java - Improved event handling in MinecraftEvents.java for player death in case of mods like Revive Me. Now the death message will be sent on player respawn rather than on-death.

--- src/main/java/com/denisnumb/discord_chat_mod/Config.java
+++ src/main/java/com/denisnumb/discord_chat_mod/Config.java
@@ -6,85 +6,99 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
 @Mod.EventBusSubscriber(modid = DiscordChatMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class Config
-{
-    private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
-    private static final ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
+public class Config {
+        public enum AchievementMessageStyle {
+                PLAINTEXT,
+                EMBED
+        }
 
-    public static final ForgeConfigSpec.ConfigValue<String> DISCORD_BOT_TOKEN = COMMON_BUILDER
-            .comment("""
-                     Bot access token\
-                    
-                     [!] Make sure all Privileged Gateway Intents are enabled on https://discord.com/developers/applications/<your_app_id>/bot\
-                    
-                     [!] Make sure the bot has the following permissions on the server:\
-                    
-                     [!] - VIEW_CHANNEL\
-                    
-                     [!] - MESSAGE_SEND\
-                    
-                     [!] - MESSAGE_SEND_IN_THREADS\
-                    
-                     [!] - MESSAGE_EMBED_LINKS\
-                    
-                     [!] - MESSAGE_ATTACH_FILES\
-                    
-                     [!] - MESSAGE_MANAGE\
-                    
-                     [!] - MESSAGE_HISTORY\
-                    """)
-            .define("discordBotToken", "");
+        private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
+        private static final ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
 
-    public static final ForgeConfigSpec.ConfigValue<String> DISCORD_CHANNEL_ID = COMMON_BUILDER
-            .comment(" Discord channel ID for messaging with MineCraft\n [!] Make sure the bot has access to the channel and all the permissions listed above.")
-            .define("discordChannelId", "");
+        public static final ForgeConfigSpec.ConfigValue<String> DISCORD_BOT_TOKEN = COMMON_BUILDER
+                        .comment("""
+                                         Bot access token\
 
-    public static final ForgeConfigSpec.BooleanValue LOG_DISCORD_MESSAGES = COMMON_BUILDER
-            .comment(" Do logging to the server console messages from discord")
-            .define("logDiscordMessages", true);
+                                         [!] Make sure all Privileged Gateway Intents are enabled on https://discord.com/developers/applications/<your_app_id>/bot\
 
-    public static final ForgeConfigSpec.BooleanValue ENABLE_PINNED_STATUS_MESSAGE = COMMON_BUILDER
-            .comment(" Create a pinned message with the current server status and player list")
-            .define("enablePinnedStatusMessage", true);
+                                         [!] Make sure the bot has the following permissions on the server:\
 
-    public static final ForgeConfigSpec.BooleanValue LOG_DISCORD_ERRORS_TO_SERVER_CHAT = COMMON_BUILDER
-            .comment(" Notify about internal Discord interaction errors in the server's in-game chat")
-            .define("logDiscordErrorsToServerChat", true);
+                                         [!] - VIEW_CHANNEL\
 
-    public static final ForgeConfigSpec.ConfigValue<String> DISCORD_ERRORS_CHAT_PLAYER_SELECTOR = COMMON_BUILDER
-            .comment(" If logDiscordErrorsToServerChat=true, then the errors in the chat will be seen by players with the specified selector" +
-                    "\n By default, \"@a\" — all players. You can specify a specific nickname or attribute, for example, \"@a[tag=admin]\"")
-            .define("discordErrorsChatPlayerSelector", "@a");
+                                         [!] - MESSAGE_SEND\
 
-    public static final ForgeConfigSpec.ConfigValue<String> MOD_LOCALE = COMMON_BUILDER
-            .comment(" Mod locale")
-            .define("modLocale", "en_us");
+                                         [!] - MESSAGE_SEND_IN_THREADS\
 
-    public static final ForgeConfigSpec.BooleanValue EMOJIFUL_COMPATIBILITY = CLIENT_BUILDER
-            .comment("""
-                     Can be set to "true" for correct compatibility with the Emojiful mod. Enabling this option:\
-                    
-                     1) Disables Discord Chat Mod's emoji suggestions to avoid conflicts with Emojiful\
-                    
-                     2) Disables Discord Chat Mod's rendering of custom Discord Emoji\
-                    
-                     3) Convert emoji sent from Discord to their names. For example, "\uD83D\uDE03" will be converted to ":smiley:"\
-                    """)
-            .define("emojifulCompatibility", false);
+                                         [!] - MESSAGE_EMBED_LINKS\
 
-    public static final ForgeConfigSpec.IntValue MAX_CHAT_HISTORY = CLIENT_BUILDER
-            .comment("""
-                     Maximum number of messages in the chat.\
-                    
-                     When the number of messages exceeds this value, old messages are automatically deleted.\
-                    
-                     By default, this value is 100 in the vanilla game.\
-                    """)
-            .defineInRange("maxChatHistory", 500, 20, Integer.MAX_VALUE);
+                                         [!] - MESSAGE_ATTACH_FILES\
 
-    static final ForgeConfigSpec COMMON_SPEC = COMMON_BUILDER.build();
-    static final ForgeConfigSpec CLIENT_SPEC = CLIENT_BUILDER.build();
+                                         [!] - MESSAGE_MANAGE\
 
-    @SubscribeEvent
-    static void onLoad(final ModConfigEvent event) {}
+                                         [!] - MESSAGE_HISTORY\
+                                        """)
+                        .define("discordBotToken", "");
+
+        public static final ForgeConfigSpec.ConfigValue<String> DISCORD_CHANNEL_ID = COMMON_BUILDER
+                        .comment(" Discord channel ID for messaging with MineCraft\n [!] Make sure the bot has access to the channel and all the permissions listed above.")
+                        .define("discordChannelId", "");
+
+        public static final ForgeConfigSpec.BooleanValue LOG_DISCORD_MESSAGES = COMMON_BUILDER
+                        .comment(" Do logging to the server console messages from discord")
+                        .define("logDiscordMessages", true);
+
+        public static final ForgeConfigSpec.BooleanValue ENABLE_PINNED_STATUS_MESSAGE = COMMON_BUILDER
+                        .comment(" Create a pinned message with the current server status and player list")
+                        .define("enablePinnedStatusMessage", true);
+
+        public static final ForgeConfigSpec.BooleanValue LOG_DISCORD_ERRORS_TO_SERVER_CHAT = COMMON_BUILDER
+                        .comment(" Notify about internal Discord interaction errors in the server's in-game chat")
+                        .define("logDiscordErrorsToServerChat", true);
+
+        public static final ForgeConfigSpec.ConfigValue<String> DISCORD_ERRORS_CHAT_PLAYER_SELECTOR = COMMON_BUILDER
+                        .comment(" If logDiscordErrorsToServerChat=true, then the errors in the chat will be seen by players with the specified selector"
+                                        +
+                                        "\n By default, \"@a\" — all players. You can specify a specific nickname or attribute, for example, \"@a[tag=admin]\"")
+                        .define("discordErrorsChatPlayerSelector", "@a");
+
+        public static final ForgeConfigSpec.ConfigValue<String> MOD_LOCALE = COMMON_BUILDER
+                        .comment(" Mod locale")
+                        .define("modLocale", "en_us");
+
+        public static final ForgeConfigSpec.EnumValue<AchievementMessageStyle> ACHIEVEMENT_MESSAGE_STYLE = COMMON_BUILDER
+                        .comment(" How to display achievement messages in Discord. Can be \"plaintext\" or \"embed\".")
+                        .defineEnum("achievementMessageStyle", AchievementMessageStyle.EMBED);
+
+        public static final ForgeConfigSpec.BooleanValue SEND_ACHIEVEMENT_DESCRIPTION = COMMON_BUILDER
+                        .comment(" Whether to include the description of the achievement in the message. Only works with 'embed' style.")
+                        .define("sendAchievementDescription", true);
+
+        public static final ForgeConfigSpec.BooleanValue EMOJIFUL_COMPATIBILITY = CLIENT_BUILDER
+                        .comment("""
+                                         Can be set to "true" for correct compatibility with the Emojiful mod. Enabling this option:\
+
+                                         1) Disables Discord Chat Mod's emoji suggestions to avoid conflicts with Emojiful\
+
+                                         2) Disables Discord Chat Mod's rendering of custom Discord Emoji\
+
+                                         3) Convert emoji sent from Discord to their names. For example, "\uD83D\uDE03" will be converted to ":smiley:"\
+                                        """)
+                        .define("emojifulCompatibility", false);
+
+        public static final ForgeConfigSpec.IntValue MAX_CHAT_HISTORY = CLIENT_BUILDER
+                        .comment("""
+                                         Maximum number of messages in the chat.\
+
+                                         When the number of messages exceeds this value, old messages are automatically deleted.\
+
+                                         By default, this value is 100 in the vanilla game.\
+                                        """)
+                        .defineInRange("maxChatHistory", 500, 20, Integer.MAX_VALUE);
+
+        static final ForgeConfigSpec COMMON_SPEC = COMMON_BUILDER.build();
+        static final ForgeConfigSpec CLIENT_SPEC = CLIENT_BUILDER.build();
+
+        @SubscribeEvent
+        static void onLoad(final ModConfigEvent event) {
+        }
 }
--- src/main/java/com/denisnumb/discord_chat_mod/MinecraftEvents.java
+++ src/main/java/com/denisnumb/discord_chat_mod/MinecraftEvents.java
@@ -16,15 +16,20 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.AdvancementEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+
 import static com.denisnumb.discord_chat_mod.ColorUtils.Color.GOLD;
 import static com.denisnumb.discord_chat_mod.ColorUtils.Color.PURPLE;
 import static com.denisnumb.discord_chat_mod.DiscordChatMod.*;
@@ -36,6 +41,8 @@ import static com.denisnumb.discord_chat_mod.discord.ServerStatusController.upda
 
 @Mod.EventBusSubscriber(modid = DiscordChatMod.MODID)
 public class MinecraftEvents {
+    private static final Map<UUID, LivingDeathEvent> pendingDeaths = new HashMap<>();
+
     @SubscribeEvent
     public static void onRegisterCommands(RegisterCommandsEvent event) {
         MentionCommand.register(event.getDispatcher());
@@ -55,35 +62,34 @@ public class MinecraftEvents {
                 if (message.contains(member.prettyMention))
                     message = message.replace(member.prettyMention, member.mentionString);
 
-            mentions = new HashMap<>(){{
-               for (DiscordMemberData member : memberData)
-                   put(member.mentionString, new DiscordMentionData(member));
-            }};
+            mentions = new HashMap<>() {
+                {
+                    for (DiscordMemberData member : memberData)
+                        put(member.mentionString, new DiscordMentionData(member));
+                }
+            };
 
-            prepareDiscordTextMessage(String.format("`<%s>` %s", event.getPlayer().getName().getString(), replaceEmojiCodesToDiscordMentions(message)))
+            prepareDiscordTextMessage(String.format("`<%s>` %s", event.getPlayer().getName().getString(),
+                    replaceEmojiCodesToDiscordMentions(message)))
                     .ifPresent(DiscordUtils::sendMessage);
         }
 
         event.setMessage(
                 new MarkdownToComponentConverter(MarkdownParser.parseMarkdown(message), mentions)
-                        .convertMarkdownTokensToComponent()
-        );
+                        .convertMarkdownTokensToComponent());
     }
 
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onPlayerDieEvent(LivingDeathEvent event) {
-        if (!isDiscordConnected())
+        if (event.isCanceled()) {
             return;
-        if (!(event.getEntity() instanceof Player))
-            return;
-
-        String message;
-        try{
-            message = getLocalizedDeathMessage(event.getSource(), event.getEntity());
-        } catch (Exception ignored) {
-            message = event.getSource().getLocalizedDeathMessage(event.getEntity()).getString();
         }
-        sendShortEmbedMessage(message, DEFAULT);
+
+        if (!(event.getEntity() instanceof Player player)) {
+            return;
+        }
+
+        pendingDeaths.put(player.getUUID(), event);
     }
 
     @SubscribeEvent
@@ -96,42 +102,73 @@ public class MinecraftEvents {
         if (!displayInfo.shouldAnnounceChat())
             return;
 
-
         String message = displayInfo.getFrame() == FrameType.TASK
                 ? getTranslate("chat.type.advancement.task")
                 : displayInfo.getFrame() == FrameType.GOAL
-                ? getTranslate("chat.type.advancement.goal")
-                : getTranslate("chat.type.advancement.challenge");
+                        ? getTranslate("chat.type.advancement.goal")
+                        : getTranslate("chat.type.advancement.challenge");
 
         ResourceLocation advancementId = event.getAdvancement().getId();
         ResourceLocation advancementResourceLocation = new ResourceLocation(
                 advancementId.getNamespace(),
-                "advancements/" + advancementId.getPath() + ".json"
-        );
+                "advancements/" + advancementId.getPath() + ".json");
 
         String title = displayInfo.getTitle().getString();
         String description = displayInfo.getDescription().getString();
 
         var advancementJson = getAdvancementFileAsJsonObject(advancementResourceLocation);
-        if (advancementJson != null){
+        if (advancementJson != null) {
             title = getTranslatedAdvancementTitle(advancementJson, title);
             description = getTranslatedAdvancementDescription(advancementJson, description);
         }
 
-        int color = displayInfo.getFrame() == FrameType.CHALLENGE ? PURPLE : GOLD;
+        String formattedPlayerName = "**" + event.getEntity().getName().getString() + "**";
         String formattedTitle = MarkdownParser.parseMarkdown(title).stream().allMatch(MarkdownToken::hasNoMarkdown)
                 ? "**`" + title + "`**"
                 : title;
 
-        sendEmbedMessage(
-                String.format(
+        if (Config.ACHIEVEMENT_MESSAGE_STYLE.get() == Config.AchievementMessageStyle.EMBED) {
+            int color = displayInfo.getFrame() == FrameType.CHALLENGE ? PURPLE : GOLD;
+            sendEmbedMessage(
+                    String.format(
+                            message,
+                            formattedPlayerName,
+                            formattedTitle),
+                    Config.SEND_ACHIEVEMENT_DESCRIPTION.get() ? description : "",
+                    color);
+        } else {
+            String plainTextMessage = String.format(
                     message,
-                    "**" + event.getEntity().getName().getString() + "**",
-                    formattedTitle
-                ),
-                description,
-                color
-        );
+                    formattedPlayerName,
+                    formattedTitle);
+            prepareDiscordTextMessage(plainTextMessage).ifPresent(DiscordUtils::sendMessage);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerClone(PlayerEvent.Clone event) {
+        if (!event.isWasDeath()) {
+            return;
+        }
+
+        Player player = event.getEntity();
+        LivingDeathEvent deathEvent = pendingDeaths.remove(player.getUUID());
+
+        if (deathEvent == null) {
+            return;
+        }
+
+        // The player has truly died and respawned. Send the message.
+        if (!isDiscordConnected())
+            return;
+
+        String message;
+        try {
+            message = getLocalizedDeathMessage(deathEvent.getSource(), deathEvent.getEntity());
+        } catch (Exception ignored) {
+            message = deathEvent.getSource().getLocalizedDeathMessage(deathEvent.getEntity()).getString();
+        }
+        sendShortEmbedMessage(message, DEFAULT);
     }
 
     @SubscribeEvent
@@ -140,8 +177,31 @@ public class MinecraftEvents {
     }
 
     @SubscribeEvent
-    public static void onPlayerLeaveEvent(PlayerEvent.PlayerLoggedOutEvent event){
+    public static void onPlayerLeaveEvent(PlayerEvent.PlayerLoggedOutEvent event) {
         joinLeaveEvent(event);
+    }
+
+    @SubscribeEvent
+    public static void onServerTick(TickEvent.ServerTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) {
+            return;
+        }
+        if (pendingDeaths.isEmpty()) {
+            return;
+        }
+
+        Iterator<Map.Entry<UUID, LivingDeathEvent>> iterator = pendingDeaths.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<UUID, LivingDeathEvent> entry = iterator.next();
+            UUID playerUuid = entry.getKey();
+            Player player = server.getPlayerList().getPlayer(playerUuid);
+
+            // Player is online and not dead (revived), so we remove them from the pending
+            // list.
+            if (player != null && !player.isDeadOrDying()) {
+                iterator.remove();
+            }
+        }
     }
 
     private static void joinLeaveEvent(PlayerEvent event) {
